43d65ccf28c5c001a2a555a973b01f5697f5c6aa
michael hohl <mhohl@rocketmail.com>
2018-05-21 20:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0, l = array.length; i < l; i++) {
-
+    if (findFn(array[i])) {
+      return array[i]
+    }
   }
+  return null
 }
